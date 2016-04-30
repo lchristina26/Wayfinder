29a9e5f89340096cd087ce5347990d3c5cb538d0
lchristina26 <leah@wolfssl.com>
2016-04-30 17:28:27 -0600

    bug fixes

--- wayfinder.ino
+++ wayfinder.ino
@@ -1,7 +1,11 @@
+#include <DW1000Time.h>
 #include <DW1000.h>
+#include <DW1000Mac.h>
 #include <DW1000Ranging.h>
-##include <DW1000Device.h>
+#include <DW1000Device.h>
+#include <digitalWriteFast.h>
 #include <SoftwareSerial.h>
+#include <string.h>
 
 /*
    Message Syntax:
@@ -13,13 +17,41 @@
 
 */
 
-#define myAddr  101  // Address of this node
 #define rxPin   4  // Serial input (connects to Emic 2's SOUT pin)
 #define txPin   5  // Serial output (connects to Emic 2's SIN pin)
 #define ledPin  13  // Most Arduino boards have an on-board LED on this pin
 #define DW_rst  9  // Reset pin for UWB
 #define DW_cs   10  // Slave select for UWB
 #define DW_int  2  // UWB interrupt pin
+#define replyDelayTimeUS 3000 // delay for 3000 ms
+#define myAddr ((const char*) "101")  // Address of this node
+#define cliAddr ((const char* ) "XXX") // need to add what this really is...
+#define LEN_DATA 4
+byte data[LEN_DATA]; // this might not be enough...
+// last computed range/time
+DW1000Time timeComputedRange;
+// timestamps to remember
+DW1000Time timePollSent;
+DW1000Time timePollReceived;
+DW1000Time timePollAckSent;
+DW1000Time timePollAckReceived;
+DW1000Time timeRangeSent;
+DW1000Time timeRangeReceived;
+
+typedef struct {
+  char direction;
+  int distance;
+} NavInstruction;
+
+NavInstruction getNavigationInstruction (int destination, int origin);
+void onUWBError();
+void receiver();
+void parseCommand ();
+int getDistanceToClient ();
+void sendCommand (int addr, int command, String args);
+void sendCommand (int addr, int command, String args);
+void computeRangeAsymmetric();
+int getRelativeVelocity ();
 
 // set up a new serial port
 SoftwareSerial emicSerial =  SoftwareSerial(rxPin, txPin);
@@ -30,16 +62,6 @@ int myClient, myClientOrigin, myClientDest, lastVelocity, offCourseCounter, time
 boolean send20 = false;
 boolean send10 = false;
 boolean send5 = false;
-
-typedef struct {
-  String direction;
-  int distance;
-} NavInstruction;
-
-// matrix of directions
-char dirs[3][3] = {('0', 'l', 'l'), ('r', '0', 'r'), ('l', 'l', '0')}; 
-// matrix of distances between nodes
-char dists[3][3] = {(0, 10, 30), (10, 0, 20), (30, 20, 0)};
 
 void setup()  // Set up code called once on start-up
 {
@@ -70,7 +92,7 @@ void setup()  // Set up code called once on start-up
   // general configuration
   DW1000.newConfiguration();
   DW1000.setDefaults();
-  DW1000.setDeviceAddress(addr);
+  DW1000.setDeviceAddress(5);
   DW1000.setNetworkId(10);
   DW1000.enableMode(DW1000.MODE_LONGDATA_RANGE_LOWPOWER);
   DW1000.commitConfiguration();
@@ -95,9 +117,13 @@ void setup()  // Set up code called once on start-up
 }
 
 void speakInstruction(int distance, String inst) {
-  while (emicSerial.read() != ":");
+  while (emicSerial.read() != ':');
   emicSerial.print('S');
-  emicSerial.print("In " + distance + " feet, " + inst + ".");
+  emicSerial.print("In ");
+  emicSerial.print(distance);
+  emicSerial.print(" feet, ");
+  emicSerial.print(inst);
+  emicSerial.print(".");
   emicSerial.print("\n");
 }
 
@@ -109,7 +135,7 @@ void handleSent() {
 // Receive error interrupt for DWM1000
 void onMsgFailure () {
   Serial.println("DWM1000 message receive error!!");
-  error = false;
+  error = "false";
   DW1000.getData(error);
   Serial.print("Error data is ... "); Serial.println(message);
 }
@@ -117,7 +143,7 @@ void onMsgFailure () {
 // Error interrupt forDW1000
 void onDW1000Error () {
   Serial.println("DWM1000 error!!");
-  error = false;
+  error = "false";
   DW1000.getData(error);
   Serial.print("Error data is ... "); Serial.println(message);
 }
@@ -133,7 +159,6 @@ void didReceiveUWBMessage() {
   Serial.print("Signal quality is ... "); Serial.println(DW1000.getReceiveQuality());
   message = "";
 }
-}
 
 void parseCommand () {
   if (sizeof(message) < 1 ) {
@@ -142,39 +167,47 @@ void parseCommand () {
   }
 
   // Parse
-  addrEnd = message.indexOf(":");
-  if (message.substring(0, addrEnd) != myAddr) return;
-  commandEnd = message.indexOf("_", addrEnd + 1);
-  command = message.substring(addrEnd + 1, commandEnd).toInt();
+  int addrEnd = message.indexOf(':');
+  const char* recAddr = message.substring(0, addrEnd).c_str();
+  if (!strcmp(recAddr, myAddr)) return;
+  int commandEnd = message.indexOf("_", addrEnd + 1);
+  int command = message.substring(addrEnd + 1, commandEnd).toInt();
   args = message.substring(commandEnd + 1);
 
   switch (command) {
     case 1:
+    {
       // Initiate navigation
       // Parse args
-      _addr = args;
-      Serial.println("***Command: init navigation. From node: " + _addr);
+      //int _addr = atoi(args.c_str());
+      Serial.print("***Command: init navigation. From node: "); 
+      Serial.println(recAddr);
       // Get distance to client, send response to client
       int _dist = getDistanceToClient();
-      sendCommand(_addr, 3, _dist);
+      sendCommand(atoi(recAddr), 3, args);
+      break;
+    }
 
     case 2:
+    {
       // Respond to CONFIRM
       // We're now the guide node for the client
       // Parse args:
       int _pos = args.indexOf(",");
-      int _addr = args.substring(0, _pos);
+      int _addr = args.substring(0, _pos).toInt();
       args.remove(0, _pos);
-      myClientDest = args;
+      myClientDest = args.toInt();
       myClient = _addr;
       Serial.println("***Command: confirmation. From client: " + myClient);
       // Send first direction message!
-      NavInstruction _inst = getNavigationInstruction(myClientDestination, myClientOrigin);
+      NavInstruction _inst = getNavigationInstruction(myClientDest, myClientOrigin);
       sendCommand(myClient, 9, _inst.direction + "," + _inst.distance);
       timeSinceInst = millis();
+      break;
+    }
   }
 }
-}
+
 
 void onUWBError () {
   Serial.println("An unknown DWM1000 error ocurred.");
@@ -182,31 +215,38 @@ void onUWBError () {
 
 void sendCommand (int addr, int command, String args) {
   Serial.println("Beginning command send...");
-  Serial.println("    Command: " + command + ": " + args);
+  Serial.print("    Command: ");
+  Serial.print(command); 
+  Serial.print(": ");
+  Serial.println(args);
   DW1000.newTransmit();
   DW1000.setDefaults();
-  DW1000.setData(addr + ":" + command + "_" + args);
+  DW1000.setData(addr + ':' + command + '_' + args);
   DW1000.startTransmit();
 }
 
 void sendCommand(int addr, NavInstruction inst) {
   Serial.println("Beginning command send...");
-  Serial.println("    Command: 9:" + inst.direction + "," + inst.distance);
+  Serial.println("    Command: 9:" + inst.direction + ',' + inst.distance);
   DW1000.newTransmit();
   DW1000.setDefaults();
-  DW1000.setData(addr + ":9_" + inst.direction + "," + inst.distance);
+  DW1000.setData(addr + ":9_" + inst.direction + ',' + inst.distance);
   DW1000.startTransmit();
 }
 
 NavInstruction getNavigationInstruction (int destination, int origin) {
   char dir;
+  // matrix of distances between nodes
+  char dists[3][3] = {(0, 10, 30), (10, 0, 20), (30, 20, 0)};
+  // matrix of directions
+  char dirs[3][3] = {('0', 'l', 'l'), ('r', '0', 'r'), ('l', 'l', '0')}; 
   NavInstruction n_i;
-  if (dirs[origin][dest] == 'l') { //
+  if (dirs[origin][destination] == 'l') { //
     n_i.direction = 'L'; // command to go left
   } else {
     n_i.direction = 'R'; // command to go right
   }
-  n_i.distance = dists[origin][dest];
+  n_i.distance = dists[origin][destination];
 
   return n_i;
 }
@@ -249,13 +289,22 @@ void transmitRangeFailed() {
 }
 
 int getDistanceToClient () {
-  DW1000Time myTOF;
-  DW1000Device *distantDevice = 
-      searchDistantDevice(_lastSentToShortAddress); // need to get addr
-
-  DW100Ranging.computeRangeAsymmetric(distantDevice, &myTOF);
-  float distance=myTOF.getAsMeters();
+  computeRangeAsymmetric(); 
+  transmitRangeReport(timeComputedRange.getAsFloat());
+  float distance=timeComputedRange.getAsMeters();
   return (int)distance;
+}
+
+
+void computeRangeAsymmetric() {
+    // asymmetric two-way ranging (more computation intense, less error prone)
+    DW1000Time round1 = (timePollAckReceived-timePollSent).wrap();
+    DW1000Time reply1 = (timePollAckSent-timePollReceived).wrap();
+    DW1000Time round2 = (timeRangeReceived-timePollAckSent).wrap();
+    DW1000Time reply2 = (timeRangeSent-timePollAckReceived).wrap();
+    DW1000Time tof = (round1 * round2 - reply1 * reply2) / (round1 + round2 + reply1 + reply2);
+    // set tof timestamp
+    timeComputedRange.setTimestamp(tof);
 }
 
 int getRelativeVelocity () {
@@ -267,26 +316,27 @@ void loop()  // Main code, to run repeatedly
 {
   // Client monitoring
   if (myClient > 0) {
-    _dist = getDistanceToClient();
-    _velocity = getRelativeVelocity();
+    int _dist = getDistanceToClient();
+    int _velocity = getRelativeVelocity();
     if (_velocity - lastVelocity < 0) {
       // Client is now moving AWAY from node
-      if (++offCourseCount > 5) {
+      if (++offCourseCounter > 5) {
         // Send OFF COURSE command to client
         sendCommand(myClient, 4, "");
       }
     } else {
-      offCourseCount = 0;
+      offCourseCounter = 0;
     }
 
     // See if we need to send an update message
     if ((_dist <= 20 && !send20) || (_dist <= 10 && !send10) || (_dist <= 5 && !send5) || millis() - timeSinceInst > 10000) {
-      NavInstruction _inst = getNavigationInstruction(myClientDestination, myClientOrigin);
+      NavInstruction _inst = getNavigationInstruction(myClientDest, myClientOrigin);
       sendCommand(myClient, 9, _inst.direction + "," + _inst.distance);
       timeSinceInst = millis();
     }
     delay(1000);
   }
+}  
 
 
 
